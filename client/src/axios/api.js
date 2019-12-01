dc35af1647dbfc5c1057c7a93f69f046d05f1ed4
pkojchevski <petarkojchevski@yahoo.com>
2019-12-01 20:36:01 +0100

    '01122019_7'

--- client/src/axios/api.js
+++ client/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://localhost:8080/"
+  baseURL: "http://localhost:8080/"
 });
 
 export const setAuthToken = token => {
