971f730fd57d9de21bb3f6e19be07152e1686b17
pkojchevski <petarkojchevski@yahoo.com>
2019-12-01 20:19:40 +0100

    '01122019_5'

--- client/src/axios/api.js
+++ client/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080/"
+  baseURL: "https://szablony.herokuapp.com/"
 });
 
 export const setAuthToken = token => {
