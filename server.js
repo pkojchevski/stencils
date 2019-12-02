a0d43b47f340e73e3243c86e562860ab41a4fdfd
pkojchevski <petarkojchevski@yahoo.com>
2019-12-02 20:17:29 +0100

    'changed_port_to_5000'

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 
 require("./controllers/passport")(passport);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Listening on:", port));
 
